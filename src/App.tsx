50237969d1bc407bf6d8c0984efe5e626da943b4
davidalves1 <alves.david@outlook.com>
2024-03-08 11:24:08 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="w-full h-[100vh] flex justify-center items-center bg-zinc-50">
+      <h1 className="text-3xl font-light">Hello!</h1>
+    </main>
+  )
+}
+
+export default App
